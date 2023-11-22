b2ecd9ea345f99c5a3529894cfbf2139b75b727a
NishanthBhoomi <105163080+NishanthBhoomi@users.noreply.github.com>
2023-11-23 00:41:37 +0530

    Added declaration for search symbol table scope function

--- Semantic/funcs.hpp
+++ Semantic/funcs.hpp
@@ -16,6 +16,8 @@ void insert_symTab(string name,string type ,int scope);
 
 symTabEnt search_symTab(string name,int scope);
 
+symTabEnt search_symTab_scope(string name,int scope);
+
 void delete_symEnt(int scope);
 
 void insert_functab(string name,bool built_in, vector<string> param_list,string ret_type);
